c95e618bf2ebe979f3d030e2cc16fe0acc0a876e
MojoisMe <2377862945@qq.com>
2024-10-19 18:03:37 +0800

    完成task3 代码

--- HW02/Prob1/task3_tmp.py
+++ HW02/Prob1/task3_tmp.py
@@ -1,16 +1,7 @@
-from params import RAND_STATE_SEED
-from model import SVCModel
+import numpy as np
 from sklearn.neighbors import NearestNeighbors
-from dataloader import DataLoader
-from datetime import datetime
-import os
 
-"""
-% 注意:
-% 1. 这个框架提供了基本的结构，您需要完成所有标记为 'pass' 的函数。
-% 2. 记得处理数值稳定性问题，例如在计算对数时避免除以零。
-% 3. 在报告中详细讨论您的观察结果和任何有趣的发现。
-"""
+
 class SMOTE(object):
     def __init__(self, X, y, N, K, random_state=None):
         self.N = N  # 每个小类样本合成样本个数
@@ -44,33 +35,28 @@ class SMOTE(object):
         return synthetic_samples, np.ones(n_synthetic_samples) * self.label
 
 
-def task3(
-    run_time,
-    data_loader: DataLoader,
-    loadpath=None,
-):
-    task_name = "task3"
-    dir_path = f"./output/{run_time}/{task_name}"
-    savepath = f"{dir_path}/svm_model.pkl"
-    output_path = f"{dir_path}/out.out"
-    curve_path = f"{dir_path}/roc_curve.png"
-
-    os.makedirs(dir_path, exist_ok=True)
-    outfile = open(output_path, "w")  # 清空文件内容
-    outfile.close()
-
-    X_train, X_test, y_train, y_test = data_loader.split(test_size=0.2, stratify=False)
-    X_train_neg = X_train[y_train == 0]
-    y_neg_label = 0
-
-    smote = SMOTE(X_train_neg, y_neg_label, N=200, K=7)
-
-    synthetic_samples, y_reduced = smote.over_sampling()
-    X_train = np.vstack([X_train, synthetic_samples])
-    y_train = np.hstack([y_train, y_reduced])
-
+# 示例使用
 if __name__ == "__main__":
-    data_loader = DataLoader("../data/creditcard.csv")
-    run_timestemp = datetime.now().strftime("%m%d_%H%M%S")
-    load_path = None
-    task3(data_loader, run_timestemp, load_path)
+    # 示例数据
+    X = np.array(
+        [
+            [1, 1, 4],
+            [1, 3, 2],
+            [2, 4, 1],
+            [3, 1, 2],
+            [1, 2, 3],
+            [2, 5, 2],
+        ]
+    )
+    oy = np.array([1, 1, 1, 0, 0, 0])
+    y = 1
+    N = 2  # 每个小类样本合成的样本个数
+    K = 2  # 近邻个数
+    random_state = 53
+
+    smote = SMOTE(X, y, N, K, random_state)
+    synthetic_samples, y_reduced = smote.over_sampling()
+    samples = np.vstack([X, synthetic_samples])
+    oy = np.hstack([oy, y_reduced])
+    print("样本:\n", samples)
+    print("样本标签:\n", oy)
